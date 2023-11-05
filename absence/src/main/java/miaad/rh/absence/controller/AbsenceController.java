3ff9c22c592351bf2f077a0f30ed2a7a0fe5e876
TOUZOUZ Adnane <adnanetouzouz7@gmail.com>
2023-11-05 22:10:40 +0100

    micro-service absence version 1.2 envoyer un email au collaborateur qui a dépacer le seil

--- absence/src/main/java/miaad/rh/absence/controller/AbsenceController.java
+++ absence/src/main/java/miaad/rh/absence/controller/AbsenceController.java
@@ -6,7 +6,10 @@ import miaad.rh.absence.entity.Absence;
 import miaad.rh.absence.service.AbsenceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.SimpleMailMessage;
 
 import java.util.List;
 
@@ -15,11 +18,33 @@ import java.util.List;
 @RequestMapping("/api/absences")
 public class AbsenceController {
     private AbsenceService absenceService;
+    private JavaMailSender javaMailSender;
 
     @PostMapping
-    public ResponseEntity<AbsenceDto> createAbsence(@RequestBody AbsenceDto absenceDto){
+    public ResponseEntity<AbsenceDto> createAbsence(@RequestBody AbsenceDto absenceDto) {
         AbsenceDto savedAbsence = absenceService.createAbsence(absenceDto);
+        Long collaborateurId = absenceDto.getCollaborateurId();
+        List<AbsenceDto> collaborateurAbsences = absenceService.getAbsenceBycollaborateurId(collaborateurId);
+        int numberOfAbsencesForCollaborateur = collaborateurAbsences.size();
+
+        if (numberOfAbsencesForCollaborateur >= 6) {
+            String message = "Vous avez dépassé le seuil des absences.";
+            sendEmailToCollaborateur(collaborateurId, message);
+        }
+
         return new ResponseEntity<>(savedAbsence, HttpStatus.CREATED);
+    }
+
+    private void sendEmailToCollaborateur(Long collaborateurId, String message) {
+        // Récupérez l'adresse e-mail du collaborateur à partir de sa base de données ou d'une autre source
+        String collaborateurEmail = "amine.rcd.ar@gmail.com";
+
+        // Envoyer l'e-mail
+        SimpleMailMessage emailMessage = new SimpleMailMessage();
+        emailMessage.setTo(collaborateurEmail);
+        emailMessage.setSubject("Dépassement du seuil d'absences");
+        emailMessage.setText(message);
+        javaMailSender.send(emailMessage);
     }
 
     @GetMapping("{collaborateurId}")
